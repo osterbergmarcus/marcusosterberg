e6687c356155a4f4a37b398a6e64b25024a19849
Marcus Osterberg <marcusosterberg.com>
2020-01-06 21:59:16 +0100

    Use webp

--- src/components/bio.js
+++ src/components/bio.js
@@ -48,7 +48,7 @@ const Bio = () => {
       avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
         childImageSharp {
           fixed(width: 50, height: 50) {
-            ...GatsbyImageSharpFixed
+            ...GatsbyImageSharpFixed_withWebp
           }
         }
       }
