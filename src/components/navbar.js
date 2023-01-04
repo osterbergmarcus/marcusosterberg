a16d3824504c837325c5620b92afd2ae15e95f4c
Marcus Osterberg <marcusosterberg.com>
2023-01-04 18:56:26 +0100

    fix navbar on mobile view

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,35 +1,35 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
 const NavBar = () => (
   <div
     style={{
-      height: "4rem",
-      top: "0px",
-      left: "0px",
-      right: "0px",
-      backgroundColor: "black",
-      display: "flex",
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "center",
+      height: '4rem',
+      top: '0px',
+      left: '0px',
+      right: '0px',
+      backgroundColor: 'black',
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
     }}
   >
     <div className="nav-bar-content">
       <Link to="/#talks" className="nav-item" itemProp="url">
-        TALKS
-      </Link>{" "}
+        Talks
+      </Link>{' '}
       <Link className="nav-item" to="/#interviews" itemProp="url">
-        INTERVIEWS
-      </Link>{" "}
+        Interviews
+      </Link>{' '}
       <Link className="nav-item" to="/#posts" itemProp="url">
-        BLOG
-      </Link>{" "}
+        Blog
+      </Link>{' '}
       <Link className="nav-item" to="/notes" itemProp="url">
-        NOTES
-      </Link>{" "}
+        Notes
+      </Link>{' '}
       <Link className="nav-item" to="/about" itemProp="url">
-        ABOUT
+        About
       </Link>
     </div>
   </div>
